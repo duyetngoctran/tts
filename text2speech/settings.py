49808a347be9072ff33b77d8878a50328ca95e26
duyetngoctran <duyetngoctran@gmail.com>
2022-11-27 03:25:59 +0700

    update

--- text2speech/settings.py
+++ text2speech/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-&w#2gjs+8**dc6%5z11g4u)r2%or025cecbzusk-#svire-4q)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
