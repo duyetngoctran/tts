e87e1627e3c4a60ec926deb8a943be48c32c8fab
duyetngoctran <duyetngoctran@gmail.com>
2022-11-27 02:21:27 +0700

    added django on heroku

--- text2speech/settings.py
+++ text2speech/settings.py
@@ -120,3 +120,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
+
+import django_on_heroku
+django_on_heroku.settings(locals())
