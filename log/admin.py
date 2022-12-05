003c4c8795d8e0f49c7459bed6c32c6e117e670b
duyetngoctran <duyetngoctran@gmail.com>
2022-12-06 04:49:34 +0700

    update

--- log/admin.py
+++ log/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Speak
 
-admin.site.register(Speak)
+class SpeakAdmin(admin.ModelAdmin):
+    list_display = ('username', 'txt', 'timestamp')
+    #list_display = [field.name for field in Setting._meta.get_fields()]
+
+admin.site.register(Speak, SpeakAdmin)
+
